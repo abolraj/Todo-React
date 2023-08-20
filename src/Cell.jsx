a0a8020861c2c51768feac1dc0948bb094760136
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-20 09:12:42 +0330

    Add Style - add styles on Cell component

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,8 +1,19 @@
 
 function Cell({children}){
+    const style = {
+        
+    }
+    const childStyle = {
+        width: '40px',
+        height: '40px',
+
+    }
+
     return (
-        <div className="ratio-1x1 text-center w-25 h-25 rounded-2">
-            {children}
+        <div className="text-center p-2" style={style}>
+            <div className="border border-primary border-3 p-2 rounded-2 p-3 d-flex align-items-center justify-content-center" style={childStyle}>
+                {children}
+            </div>
         </div>
     );
 }
