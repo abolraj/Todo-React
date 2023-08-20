34793e46182bfa44bc9a3232d2ae8829ddd609a4
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-20 10:39:36 +0330

    Add Component - Label component to show logs

---
+++ src/Label.jsx
@@ -0,0 +1,10 @@
+function Label({children}){
+
+    return (
+        <h2 className="border-bottom border-secondary border-2 text-secondary">
+            {children}
+        </h2>
+    )
+}
+
+export default Label
