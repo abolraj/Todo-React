b0b92e6ab0a3014bc6dddb6fa8c43d67983749bc
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-20 10:40:43 +0330

    Handle Logic - add loging logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import Board from './Board'
+import Label from './Label'
 
 function App() {
   const [char, setChar] = useState('a')
+  const [label, setLabel] = useState('🎮')
+
+  const sequence = `
+  Hi !
+  Welcome to Todo Game 🎮
+  Developed by Abolfazl Rajaee nasab
+  #react
+  #bootstrap
+  #html
+  #css
+  #js
+  `;
+
+  const log = (text, interval=200,clear=false, isEnded) => {
+    let i = 0
+    const interId = setInterval(() => {
+      if(i>text.length){
+        clearInterval(interId)
+        if(clear)
+          setLabel('')
+        isEnded()
+        return
+      }
+      setLabel(text.substring(0,i))
+      i++
+    }, interval);
+    return interId
+  }
+
+  useEffect(()=>{
+    
+    // log('I am fine !',100,1)
+  },[])
+
 
   return (
-    <Board/>
+    <>
+      <Label>
+        {label}
+      </Label>
+      <Board/>
+    </>
   )
 }
 
