3676e096f1ab13a568f004c283f123e1e6c42b4d
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-20 10:40:02 +0330

    Handle Logic - handle the player state on Cell

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -1,18 +1,25 @@
+import { useState } from "react";
 
 function Cell({children}){
+    let [isX, setIsX] = useState(false)
+
     const style = {
-        
     }
     const childStyle = {
+        cursor: 'pointer',
         width: '40px',
         height: '40px',
-
     }
 
+    const toggle = () => {
+        children = isX ? 'X' : 'O';
+        setIsX(!isX)
+    }
+    
     return (
-        <div className="text-center p-2" style={style}>
-            <div className="border border-primary border-3 p-2 rounded-2 p-3 d-flex align-items-center justify-content-center" style={childStyle}>
-                {children}
+        <div className="text-center p-1 h1 user-select-none m-0" style={style}>
+            <div onClick={toggle} className={"border border-3 rounded-4 p-4 p-sm-5 d-flex align-items-center align-content-center justify-content-center "+( isX ? "border-primary text-primary":"border-danger text-danger")} style={childStyle}>
+                {isX ? 'X' : 'O'}
             </div>
         </div>
     );
