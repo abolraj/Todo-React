4f509d0fa5fd91a5025f3150fe20f3541d998da2
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-20 09:14:39 +0330

    Add Board component - make the game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import Board from './Board'
 
 function App() {
   const [char, setChar] = useState('a')
 
   return (
-    <>
-    
-    </>
+    <Board/>
   )
 }
 
