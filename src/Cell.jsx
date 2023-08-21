6fdd618b48e74e28e9a11c4d34dd76ea2d394bb4
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-21 10:23:38 +0330

    Modify Style - more performance in Cell comp

--- src/Cell.jsx
+++ src/Cell.jsx
@@ -17,8 +17,8 @@ function Cell({children}){
     }
     
     return (
-        <div className="text-center p-1 h1 user-select-none m-0" style={style}>
-            <div onClick={toggle} className={"border border-3 rounded-4 p-4 p-sm-5 d-flex align-items-center align-content-center justify-content-center "+( isX ? "border-primary text-primary":"border-danger text-danger")} style={childStyle}>
+        <div className="text-center p-1 h1 user-select-none m-0 w-auto" style={style}>
+            <div onClick={toggle} className={"border border-3 rounded-4 p-4 p-sm-5 mx-auto d-flex align-items-center align-content-center justify-content-center "+( isX ? "border-primary text-primary":"border-danger text-danger")} style={childStyle}>
                 {isX ? 'X' : 'O'}
             </div>
         </div>
