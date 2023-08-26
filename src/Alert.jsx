22a92c81307e5a42cf1014fee850476e7c987227
abolraj <142489829+abolraj@users.noreply.github.com>
2023-08-26 10:03:14 +0330

    Handle Logic - add logical color style

--- src/Alert.jsx
+++ src/Alert.jsx
@@ -1,7 +1,7 @@
-function Alert({children}){
+function Alert({children, className}){
 
     return (
-        <div class="fixed-top col-lg-6 offset-lg-3 rounded-4 rounded-top-0 alert alert-primary show d-flex align-items-end " role="alert" data-bs-theme="dark">
+        <div className={"fixed-top col-lg-6 offset-lg-3 rounded-4 rounded-top-0 alert alert-primary show d-flex align-items-end border-top-0 "+className} role="alert" data-bs-theme="dark">
             {children}
         </div>
     )
