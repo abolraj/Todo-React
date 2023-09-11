7bcd7bb74a63e17f0f1fb1fb11c9fa24b7c9a135
abolraj <142489829+abolraj@users.noreply.github.com>
2023-09-11 09:52:46 +0330

    Add Style - handle animation for try again icon

--- src/WinAlert.jsx
+++ src/WinAlert.jsx
@@ -22,11 +22,11 @@ function WinAlert({ children, isWin, onClose, onPlayAgain }) {
                 <script>alert('win alert !')</script>
                 <div id="win_alert_bg" className={"win-alert-bg d-block modal-backdrop bg-info fade " + (isWin ? ' show opacity-100 bg-opacity-25 ' : '')} data-bs-theme="dark">
                 </div>
-                <div className={"h3 p-4 rounded-5 d-flex align-items-start win-alert alert alert-success alert-dismissible position-fixed col-9 col-sm-6 fade " + (isWin ? 'show' : '')} role="alert" data-bs-theme="dark">
+                <div className={"h3 p-4 pe-5 rounded-5 d-flex align-items-start win-alert alert alert-success alert-dismissible position-fixed col-10 col-sm-6 fade " + (isWin ? 'show' : '')} role="alert" data-bs-theme="dark">
                     <button onClick={_onClose.bind(this)} className="btn btn-close m-1" data-bs-dismiss="alert" aria-label="Close">
                         
                     </button>
-                    <button onClick={_onPlayAgain.bind(this)} className="btn btn-outline-primary position-absolute col-12 top-100 mt-2 start-0 p-2 rounded-5 shadow-none" data-bs-dismiss="alert">
+                    <button onClick={_onPlayAgain.bind(this)} className="btn-try btn btn-outline-primary position-absolute col-12 top-100 mt-2 start-0 p-2 rounded-5 shadow-none" data-bs-dismiss="alert">
                         <p class="m-1 d-flex justify-content-between  align-items-center ps-3 pe-1 h2 ">
                             Try Again
                             <i class="bi bi-arrow-clockwise"></i>
