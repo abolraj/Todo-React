ba5b07bad4b1e744d6ba507839375fa951970500
abolraj <142489829+abolraj@users.noreply.github.com>
2023-09-11 09:41:01 +0330

    Handle Logic - handle reset board and fix some issues

--- src/Board.jsx
+++ src/Board.jsx
@@ -2,10 +2,9 @@ import { useEffect, useState } from "react";
 import Cell from "./Cell";
 
 function Board({isOnPlay, playerOrder, onWin, hasSystem}) {
-    const style = {
+    const [loadId, setLoadId] = useState(0)
 
-    }
-
+    const style = {}
     let cellsIsX = [
         2, 2, 2,
         2, 2, 2,
@@ -29,20 +28,24 @@ function Board({isOnPlay, playerOrder, onWin, hasSystem}) {
 
     const answerAsSystem = () => {
         // always system choice is X !
+        console.log('guessing...')
         const fills = []
         for(let line of winPositions){
             const xes = line.filter(i=>cellsIsX[i][0]===1)
             const xNo = xes.length
             const yNo = line.filter(i=>cellsIsX[i][0]===0).length
             if(Math.random()<.9 && yNo === 2 && xNo === 0) return line.find(v=>cellsIsX[v][0]===2)
-            // if(yNo > 0) continue
-            if(xNo === 2) return line.find(v=>cellsIsX[v][0]===2)
+            if(yNo + xNo === 3) continue
+            if(xNo === 2 && yNo === 0) return line.find(v=>cellsIsX[v][0]===2)
             fills.push({line, xNo, yNo})
         }
 
         fills.sort((a,b) => b.xNo > a.xNo || a.yNo > b.yNo  ? -1 : 1)
 
         const choice = fills[0]
+
+        console.log('fills:')
+        console.log(fills)
         return choice?.line && choice.line?.find(v=>cellsIsX[v][0]===2)
     }
 
@@ -66,7 +69,7 @@ function Board({isOnPlay, playerOrder, onWin, hasSystem}) {
     const load = ()=>{
         const queue = [0,1,2,5,4,3,6,7,8,5,4,3]
         const loadId = setInterval(()=>{
-            if(queue.length===0){
+            if(isOnPlay || queue.length===0){
                 clearInterval(loadId)
                 return
             }
@@ -77,20 +80,55 @@ function Board({isOnPlay, playerOrder, onWin, hasSystem}) {
             
         },100)
 
+        return loadId
+    }
+
+    const clearBoard = () => {
+        cellsIsX.map(v=>{
+            v[1](2)
+            v[0]=2
+
+            return v
+        })
+        
     }
 
     useEffect(()=>{
-        onWin(checkWinner(+!playerOrder))
-        if(hasSystem && playerOrder === 1){
+        document.addEventListener('onPlayAgain', ()=>{
+            console.log('onPLayAgain from Board comp...')
+            clearBoard()
+        })
+    },[])
+
+    useEffect(()=>{
+        if(!isOnPlay){
+            setLoadId(load())
+        }else if(loadId){
+            clearBoard()
+            clearInterval(loadId)
+        }
+    },[isOnPlay])
+
+    useEffect(()=>{        
+        if(!isOnPlay)return
+        if(loadId){
+            clearInterval(loadId)
+            setLoadId(0)
+            clearBoard()
+        }
+        let winner
+        onWin(winner = checkWinner(+!playerOrder))
+        if(winner === -1 && hasSystem && playerOrder === 1){
             let systemChoice = answerAsSystem()
             if(systemChoice !== undefined){
                 setTimeout(()=>{
-
                     document.querySelectorAll('.board .cell')[systemChoice].click()
                 },Math.max(Math.random()*1000, 200))
             }
         }
+        
     },[playerOrder])
+
 
     return (
         <div className={"board container justify-content-center align-items-center align-content-center text-center " + (isOnPlay && !(hasSystem && playerOrder===1) ?'':'pe-none')} style={style}>
