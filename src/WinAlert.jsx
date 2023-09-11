3fafbabb16f11d2c9a492ee7acf4a9c2c5d949c7
abolraj <142489829+abolraj@users.noreply.github.com>
2023-09-11 09:42:23 +0330

    Handle Logic - add try again logic

--- src/WinAlert.jsx
+++ src/WinAlert.jsx
@@ -1,18 +1,40 @@
-function WinAlert({ children, isWin, onClose }) {
+function WinAlert({ children, isWin, onClose, onPlayAgain }) {
 
     const _onClose = () => {
-        document.querySelector('#win_alert_bg').remove()
+        console.log('_onClose')
+        document.querySelector('#win_alert_bg').style.display = 'none'
         onClose()
+    }
+
+    const _onPlayAgain = (e) => {
+        console.log('_onPlayAgain')
+
+        document.querySelector('#win_alert_bg').style.display = 'none'
+
+        let event = new CustomEvent('onPlayAgain', e)
+        document.dispatchEvent(event)
+        onPlayAgain && onPlayAgain(e)
     }
 
     if(isWin)
         return (
             <>
-                <div id="win_alert_bg" className={"win-alert-bg modal-backdrop bg-info fade " + (isWin ? ' show opacity-100 bg-opacity-25 ' : '')} data-bs-theme="dark">
+                <script>alert('win alert !')</script>
+                <div id="win_alert_bg" className={"win-alert-bg d-block modal-backdrop bg-info fade " + (isWin ? ' show opacity-100 bg-opacity-25 ' : '')} data-bs-theme="dark">
                 </div>
-                <div className={"h3 d-flex align-items-end win-alert alert alert-success alert-dismissible position-fixed col-9 col-sm-6 fade " + (isWin ? 'show' : '')} role="alert" data-bs-theme="dark">
-                    <button onClick={_onClose.bind(this)} className="btn btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    {children}
+                <div className={"h3 p-4 rounded-5 d-flex align-items-start win-alert alert alert-success alert-dismissible position-fixed col-9 col-sm-6 fade " + (isWin ? 'show' : '')} role="alert" data-bs-theme="dark">
+                    <button onClick={_onClose.bind(this)} className="btn btn-close m-1" data-bs-dismiss="alert" aria-label="Close">
+                        
+                    </button>
+                    <button onClick={_onPlayAgain.bind(this)} className="btn btn-outline-primary position-absolute col-12 top-100 mt-2 start-0 p-2 rounded-5 shadow-none" data-bs-dismiss="alert">
+                        <p class="m-1 d-flex justify-content-between  align-items-center ps-3 pe-1 h2 ">
+                            Try Again
+                            <i class="bi bi-arrow-clockwise"></i>
+                        </p>
+                    </button>
+                    <div class="d-flex align-items-end">
+                        {children}
+                    </div>
                 </div>
 
             </>
